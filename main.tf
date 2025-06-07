852e820cb087f2e02364b2606c5ddf7fd17e43a3
Joao Marcelo <joaomarcelo86.iphone@gmail.com>
2025-06-07 18:06:05 +0000

    change vnc02

--- main.tf
+++ main.tf
@@ -5,3 +5,10 @@ module "my_vcn01" {
   compartment_id = var.compartment_ocid
   display_name = var.vcn01_display_name
 }
+module "my_vcn02" {
+  source = "./vcn_module"
+  cidr_block = "172.16.0.0/16"
+  dns_label = "vcn02"
+  compartment_id = var.compartment_ocid
+  display_name = "vcn02"  
+}
